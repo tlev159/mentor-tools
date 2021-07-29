af5eac9d1a9f91550d02f20e75a03a73882c48d6
tlev159 <t.levente1598@gmail.com>
2021-07-29 11:26:05 +0200

    add valid annotation to methodparameter

--- src/main/java/com/training360/mentortools/trainingClass/TrainingClassController.java
+++ src/main/java/com/training360/mentortools/trainingClass/TrainingClassController.java
@@ -3,10 +3,10 @@ package com.training360.mentortools.trainingClass;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -18,7 +18,8 @@ public class TrainingClassController {
 
     @PostMapping
     @Operation(summary = "Add new training class", description = "Ad new T360 class")
-    public TrainingClassDTO createClass(@RequestBody CreateTrainingClassCommand command) {
+    public TrainingClassDTO createClass(@Valid @RequestBody CreateTrainingClassCommand command) {
         return service.createTrainingClass(command);
     }
+
 }
