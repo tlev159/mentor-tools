762b5eca3372ccae8362c137f8ad9f0c471301b7
tlev159 <t.levente1598@gmail.com>
2021-07-29 15:11:32 +0200

    edit table name

--- src/main/resources/db/migration/V1__training.sql
+++ src/main/resources/db/migration/V1__training.sql
@@ -1,9 +1,9 @@
-CREATE TABLE training_class (
+CREATE TABLE training_classes (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   start_date DATE,
   end_date DATE
 );
 
-INSERT INTO training_class (name, start_date, end_date) VALUES ('Struktúraváltó', '2021-01-01', '2021-02-02');
-INSERT INTO training_class (name, start_date, end_date) VALUES ('Haladó', '2021-04-01', '2021-07-02');
+INSERT INTO training_classes (name, start_date, end_date) VALUES ('Struktúraváltó', '2021-01-01', '2021-02-02');
+INSERT INTO training_classes (name, start_date, end_date) VALUES ('Haladó', '2021-04-01', '2021-07-02');
