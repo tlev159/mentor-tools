87098c0db3c2f2c40ac69c792259b886443db5b9
tlev159 <t.levente1598@gmail.com>
2021-07-29 17:13:03 +0200

    edit update test for correct function

--- src/test/java/com/training360/mentortools/StudentRestTemplateIT.java
+++ src/test/java/com/training360/mentortools/StudentRestTemplateIT.java
@@ -2,6 +2,7 @@ package com.training360.mentortools;
 
 import com.training360.mentortools.student.CreateStudentCommand;
 import com.training360.mentortools.student.StudentDTO;
+import com.training360.mentortools.student.UpdateStudentCommand;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -51,22 +52,24 @@ public class StudentRestTemplateIT {
         StudentDTO studentDTO =
                 template.postForObject("/api/students", new CreateStudentCommand("Minta Eszter", "minta.eszter@gmail.com", "mintaE", ""), StudentDTO.class);
 
+        long id = studentDTO.getId();
+
         assertThat(studentDTO).extracting(StudentDTO::getName)
                 .isEqualTo("Minta Eszter");
 
-        template.postForObject("/api/students", new CreateStudentCommand("Minta Fruzsina", "minta.fruzsina@gmail.com", "mintaF", ""), StudentDTO.class);
+        template.put("/api/students/" + id, new UpdateStudentCommand("Minta Fruzsina", "minta.fruzsina@gmail.com", "mintaF", ""), StudentDTO.class);
 
-        List<StudentDTO> studentDTOS = template.exchange("/api/students",
+        StudentDTO loadedStudent = template.exchange("/api/students/" + id,
                 HttpMethod.GET,
                 null,
-                new ParameterizedTypeReference<List<StudentDTO>>() {
+                new ParameterizedTypeReference<StudentDTO>() {
                 }).getBody();
 
-        assertThat(studentDTOS)
-                .hasSize(2)
+        assertThat(loadedStudent)
                 .extracting(StudentDTO::getName)
-                .containsExactly("Minta Eszter", "Minta Fruzsina");
+                .isEqualTo("Minta Fruzsina");
 
     }
+
 
 }
