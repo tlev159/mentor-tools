2698bd1a0924b54e5a4f8c4ca26ffe057d6486d7
tlev159 <t.levente1598@gmail.com>
2021-07-30 21:28:50 +0200

    create schema for syllabus and add foreign key to training class table

--- src/main/resources/db/migration/V4__syllabus.sql
+++ src/main/resources/db/migration/V4__syllabus.sql
@@ -1,6 +1,10 @@
 CREATE TABLE syllabus (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  training_class_id BIGINT NOT NULL,
-  CONSTRAINT fk_training_class_id FOREIGN KEY (training_class_id) REFERENCES `mentor-tools`.training_classes (id) ON UPDATE CASCADE ON DELETE CASCADE
+  name VARCHAR(255) NOT NULL
+#   training_class_id BIGINT NOT NULL,
+#   CONSTRAINT fk_training_class_id FOREIGN KEY (training_class_id) REFERENCES `mentor-tools`.training_classes (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+ALTER TABLE training_classes
+    ADD syllabus_id BIGINT(20),
+    ADD CONSTRAINT syllabus_id FOREIGN KEY (syllabus_id) REFERENCES `mentor-tools`.syllabus (id) ON UPDATE CASCADE ON DELETE CASCADE;
