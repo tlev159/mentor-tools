6e4a1a4c5c093ee899391a09c7d8f3cff06c26fe
tlev159 <t.levente1598@gmail.com>
2021-07-30 12:51:43 +0200

    update create table registration

--- src/main/resources/db/migration/V3__registration.sql
+++ src/main/resources/db/migration/V3__registration.sql
@@ -1,16 +1,13 @@
-CREATE TABLE `registration` (
-  id BIGINT(20) NOT NULL AUTO_INCREMENT,
-  registrations_type VARCHAR(20) NOT NULL COLLATE 'utf8mb4_general_ci',
-  studentId BIGINT(20) NOT NULL,
-  classId BIGINT(20) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `studentId` (`studentId`) USING BTREE,
-  INDEX `classId` (`classId`) USING BTREE,
-  CONSTRAINT `FK_classId` FOREIGN KEY (`classId`) REFERENCES `mentor-tools`.`training_classes` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT `FK_stundentId` FOREIGN KEY (`studentId`) REFERENCES `mentor-tools`.`students` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+CREATE TABLE `registrations` (
+  id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  status VARCHAR(20) NOT NULL COLLATE 'utf8mb4_general_ci',
+  student_id BIGINT(20) NOT NULL,
+  training_class_id BIGINT(20) NOT NULL,
+  CONSTRAINT `fk_class_id` FOREIGN KEY (training_class_id) REFERENCES `mentor-tools`.`training_classes` (id) ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT `fk_student_id` FOREIGN KEY (student_id) REFERENCES `mentor-tools`.`students` (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
-INSERT INTO registration (registrations_type, studentId, classId) VALUES ('Active', 1, 2);
-INSERT INTO registration (registrations_type, studentId, classId) VALUES ('Exit_In_Progress', 2, 2);
-INSERT INTO registration (registrations_type, studentId, classId) VALUES ('Exit', 3, 1);
+#INSERT INTO registrations (status, student_id, training_class_id) VALUES ('Active', 1, 2);
+#INSERT INTO registrations (status, student_id, training_class_id) VALUES ('Exit_In_Progress', 2, 2);
+#INSERT INTO registrations (status, student_id, training_class_id) VALUES ('Exit', 3, 1);
