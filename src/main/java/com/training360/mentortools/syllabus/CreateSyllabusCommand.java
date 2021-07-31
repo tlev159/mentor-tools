9a103b237f5098008a0996eac507cfab04491b9d
tlev159 <t.levente1598@gmail.com>
2021-08-01 00:15:55 +0200

    add validation

--- src/main/java/com/training360/mentortools/syllabus/CreateSyllabusCommand.java
+++ src/main/java/com/training360/mentortools/syllabus/CreateSyllabusCommand.java
@@ -4,13 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateSyllabusCommand {
 
+    @NotEmpty
+    @Column(nullable = false)
     private String name;
 
+    @NotEmpty
+    @Column(nullable = false)
     private Long trainingClassId;
 
 }
